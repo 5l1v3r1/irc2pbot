fc7e7f6eec57f8cf216561b2aa4c1baa5ee0bd44
kurtcc <pritzel@gmail.com>
2014-01-05 13:23:06 +0000

    Refactoring the code.

--- sql.pl
+++ sql.pl
@@ -1,10 +1,10 @@
 #!/usr/bin/perl -w
 
-# Todo:
-# CURDATE should save time accurate to minute and seconds
-# Use all variables.
+use strict;
+use DBI();
  
- 
+# I can write a function to change these values but for now
+# they will stay the same for development.
 my $db ="urldatabase";
 my $user = "root";
  
@@ -19,19 +19,32 @@ my $channel = '#yossssu';
 my $url = 'https://www.som.com/';
 
 
-
-  use strict;
-  use DBI();
-
+# To call:
+# my $dbh = connectToMySQL();
+sub connectToMySQL {
   # Connect to the database.
   my $dbh = DBI->connect("DBI:mysql:database=$db;host=$host",
                          "$user","$pass" ,
                          {'RaiseError' => 1});
+ return $dbh;
+ }
 
-  # INSERT some data into 'foo'. We are using $dbh->quote() for
-  # quoting the name.
-  $dbh->do("INSERT INTO urls(url,channel,date_posted)  VALUES ('$url','$channel',NOW());");
 
-  # NOW(); is a mysql function that returns current date and time
-  # Disconnect from the database.
-  $dbh->disconnect();
+sub addURLToDB {
+# INSERT some data into 'foo'. We are using $dbh->quote() for
+# quoting the name.
+$dbh->do("INSERT INTO urls(url,channel,date_posted)  VALUES ('$url','$channel',NOW());");
+
+# NOW(); is a mysql function that returns current date and time
+}
+
+# Disconnect from the database.
+# my $dbh = connectToMySQL();
+# disConnectMySQL($dbh);
+#
+sub disConnectMySQL {
+my $dbh = $_[0];
+$dbh->disconnect();
+
+
+}
