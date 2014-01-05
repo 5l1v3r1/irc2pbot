13b8e81588b5c604bb65159b69a0d9522c310f5d
kurtcc <pritzel@gmail.com>
2014-01-05 08:17:54 +0000

    Renamed sql2.pl to sql.pl

--- sql.pl
+++ sql.pl
@@ -1,34 +1,41 @@
 #!/usr/bin/perl -w
  
-use DBI;
-use strict;
  
-print "Content-type: text/html\n\n";
  
 my $db ="urldatabase";
 my $user = "root";
  
 ## mysql database password
-my $pass = "jg&SH#2C.n=j9vHxzePMXAjeJ";
+my $pass = "";
  
 ## user hostname : This should be "localhost" but it can be diffrent too
 my $host="localhost";
  
 ## SQL query
-my $channel = "#help";
-my $url = "https://www.duckduckgo.com";
+my $channel = '#foo';
+my $url = 'https://www.duckduckgo.com/blahblah.html';
 
 
-my $query = "insert into urls(url,channel,date_posted) value($url,$channel,CURDATE());";
- 
-my $dbh = DBI->connect("DBI:mysql:$db:$host", $user, $pass);
-my $sqlQuery  = $dbh->prepare($query)
-or die "Can't prepare $query: $dbh->errstr\n";
- 
-my $rv = $sqlQuery->execute
-or die "can't execute the query: $sqlQuery->errstr";
- 
-}
- 
-my $rc = $sqlQuery->finish;
-exit(0);
+
+  use strict;
+  use DBI();
+
+  # Connect to the database.
+  my $dbh = DBI->connect("DBI:mysql:database=urldatabase;host=localhost",
+                         "root", "jg&SH#2C.n=j9vHxzePMXAjeJ",
+                         {'RaiseError' => 1});
+
+  # INSERT some data into 'foo'. We are using $dbh->quote() for
+  # quoting the name.
+  $dbh->do("INSERT INTO urls(url,channel,date_posted)  VALUES ('$url','$channel',CURDATE());");
+
+  # Same thing, but using placeholders
+
+  # Now retrieve data from the table.
+  my $sth = $dbh->prepare("SELECT * FROM urls");
+  $sth->execute();
+  
+  $sth->finish();
+
+  # Disconnect from the database.
+  $dbh->disconnect();
