36cd709794eb7a16df4538da0d10f9f80f0c7d34
kurtcc <pritzel@gmail.com>
2014-01-06 18:23:09 +0000

    This is just a usage example Ill remove it later on.

---
+++ 2.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl -w
+use strict;
+
+use mysql qw(:DEFAULT);
+
+my $url = "https://www.foo.com";
+my $channel = "#whoogirls";
+
+my $dbh = connectToMySQL();
+addURLToDB($dbh,$url,$channel);
+disConnectMySQL($dbh);
+
+
+
