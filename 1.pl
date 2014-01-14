d9fb230af19bc227274177b2be75dc2c6bee069e
kurtcc <pritzel@gmail.com>
2014-01-14 20:20:14 +0000

    Make sure Tor is running. Everything works now. Yay. I need a subroutine to check if tor is running or else start tor.

--- 1.pl
+++ 1.pl
@@ -4,10 +4,13 @@ use strict;
 #use warnings;
 
 
-use irc qw(:DEFAULT);
 use web qw(:DEFAULT);
 #use mysql qw(:DEFAULT);
-use base qw(Bot::BasicBot);
+
+package irc2pbot;
+#
+use base qw(Bot::BasicBot);         
+# Need to subclass Bot::BassicBot so that you can override said() routine
 
 # Add second nick later for if nick is reserved
 # add password to authenticate nick 
@@ -17,11 +20,42 @@ use base qw(Bot::BasicBot);
 # authenticate for nick to nickserv again
 # Add method to check if tor is running?
  
+  sub said {
+my ($self, $message) = @_;
+my $input = $message->{body};
+my $channel = $message->{channel};
 
+
+my ($how_many_found,$url) = web::checkForURL($input);
+if ($how_many_found > 0) {
+	# Okay we got a URL
+	# Now add it to database.
+#	my $dbh = connectToMySQL;
+#	addURLToDB($dbh,$url,$channel);
+#	disConnectMySQL($dbh);
+
+
+	# Is it an eepsite? If yes do something else get title over tor
+	# which assumes that site is either .onion or clearnet site.
+my ($how_many_eep,$title) = web::isEepSite($url);  # Will print title of eepsite
+if ($how_many_eep) {
+ return $title;
+}
+elsif (!$how_many_eep) {  # If this is not an eepsite
+#     print "This is not an eepsite";
+my $title = web::getStuffOverTor($url);
+#printTitle($socket,$channel,$title,$url);
+# Replace with self->say
+
+return "$title\n";
+
+}
+}
+}
 # Create an instance of the bot and start it running. Connect
 # to the main perl IRC server, and join some channels.
-my $bot = Bot::BasicBot->new(
-
+my $bot = irc2pbot->new(  # To use the said method that overrides Bot::BasicBot 
+	                      # We create object of irc2pbot that will have the overrides said method
     server => "127.0.0.1",
     port   => "6668",
     channels => ["#testbot"],
@@ -37,44 +71,13 @@ my $bot = Bot::BasicBot->new(
 
 
 
-#This is the main method that you'll want to override in your subclass 
-#it's the one called by default whenever someone says anything that we can hear, either in a 
-#public channel or to us in private that we shouldn't ignore.
-sub said {
-my ($self, $message) = @_;
-my $input = $message->{body};
-my $channel = $message->{channel};
-
-
-
-my ($how_many_found,$url) = checkForURL($input);
-if ($how_many_found > 0) {
-	# Okay we got a URL
-	# Now add it to database.
-#	my $dbh = connectToMySQL;
-#	addURLToDB($dbh,$url,$channel);
-#	disConnectMySQL($dbh);
-
-
-	# Is it an eepsite? If yes do something else get title over tor
-	# which assumes that site is either .onion or clearnet site.
-#my ($how_many_eep,$title) = isEepSite($url);  # Will print title of eepsite
-#if ($how_many_eep) {
-# return $title;
-#}
-#elsif (!$how_many_eep) {  # If this is not an eepsite
-#     print "This is not an eepsite";
-#my $title = getStuffOverTor($url);
-#printTitle($socket,$channel,$title,$url);
-# Replace with self->say
-
-#Um get this stupid bot to respond it can connect so far.
-return "Hello World\n";
-}
-}
 
 $bot->run();
 
+ 
+#This is the main method that you'll want to override in your subclass 
+#it's the one called by default whenever someone says anything that we can hear, either in a 
+#public channel or to us in private that we shouldn't ignore.
 
 
 # Bot can be shut down with:
